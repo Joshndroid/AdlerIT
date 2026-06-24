ddbd4c34e7d907019fa320851b64f0b616fa3b5f
Joshndroid <5584652+Joshndroid@users.noreply.github.com>
2026-06-25 09:54:38 +1000

    Fix test workflow runner issue

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{
     process::ExitCode,
 };
 
-use anyhow::{bail, Context, Result};
+use anyhow::{Context, Result, bail};
 use clap::{Parser, ValueEnum};
 
 #[derive(Clone, Copy, Debug, ValueEnum)]
